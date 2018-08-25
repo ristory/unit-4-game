592f72ac8a1caf9cddc0cbdbd3009da35b91a184
hoang <hoangcao@mail.weber.edu>
2018-08-25 00:55:43 -0600

    add function

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var random = Math.floor(Math.random() * 100 + 10); 
+var crystal1 = Math.floor(Math.random() * 30 + 1); 
+var crystal2 = Math.floor(Math.random() * 20 + 1); 
+var crystal3 = Math.floor(Math.random() * 40 + 1); 
+var crystal4 = Math.floor(Math.random() * 10 + 1); 
+console.log(random)
+$("#randomnumber").text(random);
